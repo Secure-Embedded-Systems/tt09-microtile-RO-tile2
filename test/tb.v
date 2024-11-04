cfd4ed426e9e7596a2a4b8f5982d59389c0ba28a
Arna Roy <arnaroy@autoreg-6686517.dyn.wpi.edu>
2024-11-04 14:35:35 -0500

    finale

--- test/tb.v
+++ test/tb.v
@@ -31,7 +31,7 @@ module tb ();
   assign vgnd = 1'b0;
 
   // Instantiate the module
-  tt_um_roy1707018_ro22 user_project (
+  tt_um_roy1707018_ro2 user_project (
 
     // Include power ports for the Gate Level test:
     `ifdef GL_TEST
