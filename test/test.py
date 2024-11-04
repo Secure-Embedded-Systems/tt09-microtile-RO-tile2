a84094c6f26ea57bb1e4ee45b54531aa07b405c1
Arna Roy <arnaroy@Arnas-Laptop.local>
2024-11-04 04:30:52 -0500

    update

--- test/test.py
+++ test/test.py
@@ -5,43 +5,25 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
-
 @cocotb.test()
 async def test_project(dut):
-    dut._log.info("Start")
+    dut._log.info("Starting test")
 
-    # Set the clock period to 10 us (100 KHz)
+    # Set up the clock with a period of 10 us (100 KHz)
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
     # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
+    dut._log.info("Resetting device")
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    # Test with different `ui_in` values
+    for val in [3, 2, 165, 229]:
+        dut.ui_in.value = val
+        await ClockCycles(dut.clk, 2)
+        dut._log.info(f"ui_in: {val}, TDC Output: {dut.uo_out.value}")
 
-    await ClockCycles(dut.clk,1)
-    dut._log.info("TDC check")
-    dut.ui_in.value = 3
-    await ClockCycles(dut.clk,1)
-    dut.ui_in.value = 2
-    # Read and log the output value
-    output_value = dut.uo_out.value
-    dut._log.info(f"TDC Output value: {output_value}")
+    dut._log.info("Test complete")
 
-    await ClockCycles(dut.clk,2)
-    dut._log.info("TDC check")
-    dut.ui_in.value = 165
-    await ClockCycles(dut.clk,2)
-    dut.ui_in.value = 229
-    # Read and log the output value
-    output_value = dut.uo_out.value
-    dut._log.info(f"TDC Output value: {output_value}")
-    await ClockCycles(dut.clk, 4)
-
-    
