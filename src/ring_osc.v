0371f228a12860cf61a0a0d9a558900222fd3ff2
Roy1707018 <aroy1@wpi.edu>
2024-11-08 20:19:03 -0500

    update

--- src/ring_osc.v
+++ src/ring_osc.v
@@ -1,32 +1,30 @@
-module ring_osc(input wire  rst_n,
-          input wire  clk,
-          input wire  ro_activate,
-          output wire   ro_out);
+module ring_osc(
+    input wire rst_n,
+    input wire clk,
+    input wire ro_activate,
+    output wire ro_out
+);
 
-   reg                en;
-   (*keep = "true" *) wire q;
-   (*keep = "true" *) wire q0;
-   (*keep = "true" *) wire q1;
-   (*keep = "true" *) wire q2;
-   (*keep = "true" *) wire q3;
-   (*keep = "true" *) wire q4;
-   (* keep = "true" *) wire cq4;
+    reg en;
+    (* keep = "true" *) wire q;
+    (* keep = "true" *) wire q0;
+    (* keep = "true" *) wire q1;
+    (* keep = "true" *) wire q2;
+    (* keep = "true" *) wire cq2;
 
-   
-   assign ro_out = en ? q : 1'b0;
-   
-   always @(posedge clk ) begin
-    if (ro_activate)
-        en <= 1'b1;
-    else
-        en <= 1'b0;
+    assign ro_out = en ? q : 1'b0;
+
+    always @(posedge clk) begin
+        if (ro_activate)
+            en <= 1'b1;
+        else
+            en <= 1'b0;
     end
 
-   assign cq4 = (en & q4);
-   (* keep = 1 *) cinv cinv1(.a(cq4),.q(q0));
-   (* keep = 1 *) cinv cinv2(.a(q0), .q(q1));
-   (* keep = 1 *) cinv cinv3(.a(q1), .q(q2));
-   (* keep = 1 *) cinv cinv4(.a(q2), .q(q3));
-   (* keep = 1 *) cinv cinv5(.a(q3), .q(q4));
-   assign q  = q4;
+    assign cq2 = (en & q2);
+    (* keep = 1 *) cinv cinv1(.a(cq2), .q(q0));
+    (* keep = 1 *) cinv cinv2(.a(q0), .q(q1));
+    (* keep = 1 *) cinv cinv3(.a(q1), .q(q2));
+    assign q = q2;
 endmodule
+
