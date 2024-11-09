b88959314c86822640643b473af852c29c4eb00b
Roy1707018 <aroy1@wpi.edu>
2024-11-08 20:35:38 -0500

    update

--- src/ring_osc_buffer.v
+++ src/ring_osc_buffer.v
@@ -3,14 +3,14 @@ module ring_osc_buffer (
     input wire clk,
     input wire ro_activate_1,
     input wire ro_activate_2,
-    output wire [7:0] ro1_out,  // Outputs from 16 ROs in RO1
-    output wire [7:0] ro2_out   // Outputs from 16 ROs in RO2
+    output wire [15:0] ro1_out,  // Outputs from 16 ROs in RO1
+    output wire [15:0] ro2_out   // Outputs from 16 ROs in RO2
 );
 
    // Instantiate 16 ROs for each set (RO1 and RO2), totaling 32 ROs
    genvar i;
    generate
-       for (i = 0; i <8 ; i = i + 1) begin : ro_gen
+       for (i = 0; i <16 ; i = i + 1) begin : ro_gen
            // RO1 instantiation
            ring_osc ro1 (
                .rst_n(rst_n),
